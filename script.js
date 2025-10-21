b780853db6d324239139790eaeee55a04c1ea576
Komronbek456 <komronbektohirov@gmail.com>
2025-10-21 20:52:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+prompt("Ismingizni kiriting")
+
+prompt("Familiyangizni kiriting")
+
+prompt("Yoshingizni kiriting")
+
+prompt("Yoqtirgan faningizni yozing")
+
+prompt("Yashaydigan tumaningizni kiriting")
+
+alert("malumotlaringizni kiritganingiz uchun tashakkur")
+
+console.log
